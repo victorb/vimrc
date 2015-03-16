485094af5256953dc5295c58e07f338e386b3ed0
Victor Bjelkholm <victor@Victors-MacBook-Pro-2.local>
2015-03-16 14:45:13 +0100

    Add vundle back again

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -5,8 +5,13 @@ mv ~/.vimrc ~/.vimrc.bak
 mv ~/.vim/ ~/.vim.bak/
 
 echo "## Setting up symlinks"
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/vim ~/.vim
+ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/vim $HOME/.vim
+
+echo "## Installing Vundle"
+
+git submodule init
+git submodule update
 
 echo "## Installing plugins"
 
