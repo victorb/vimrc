758639280f70e3e46dc4b9b39d019760a0864882
Victor Bjelkholm <victorbjelkholm@gmail.com>
2015-03-16 14:18:09 +0100

    Add echos

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -4,5 +4,8 @@ echo "## Backing up current VIM config"
 mv ~/.vimrc ~/.vimrc.bak
 mv ~/.vim/ ~/.vim.bak/
 
+echo "## Setting up symlinks"
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/vim ~/.vim
+
+echo "## Installation done... Enjoy"
